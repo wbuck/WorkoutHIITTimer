0ede39ca0cb089f0d4db53f014068fdcd793b179
Warren Buckley <wbuckley@warrens-mbp.home>
2018-01-11 17:38:10 -0500

    Add custom main menu cell

--- WorkoutHIITTimer/Controllers/MainMenuTableViewController.swift
+++ WorkoutHIITTimer/Controllers/MainMenuTableViewController.swift
@@ -7,19 +7,19 @@
 //
 
 import UIKit
+import ChameleonFramework
 
 class MainMenuTableViewController: UITableViewController {
     
-    let mainMenuCell = "MainMenuCell"
-
+    let mainMenuCellId = "MainMenuCell"
+    let menuOptions = ["My Timers", "Round Timer", "EMOM Timer", "Stopwatch", "TABATA Timer", "Interval Timer"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        
+        
+        tableView.register(UINib(nibName: "MainMenuTableViewCell", bundle: nil), forCellReuseIdentifier: mainMenuCellId)
     }
 
     override func didReceiveMemoryWarning() {
@@ -29,25 +29,17 @@ class MainMenuTableViewController: UITableViewController {
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return menuOptions.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: mainMenuCellId, for: indexPath) as! MainMenuTableViewCell
+        cell.timerLabel.text = menuOptions[indexPath.row].uppercased()
         return cell
     }
-    */
+ 
 
     /*
     // Override to support conditional editing of the table view.
---
+++ WorkoutHIITTimer/Custom Cell/MainMenuTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MainMenuTableViewCell.swift
+//  WorkoutHIITTimer
+//
+//  Created by Warren Buckley on 2018-01-10.
+//  Copyright © 2018 Warren Buckley. All rights reserved.
+//
+
+import UIKit
+
+class MainMenuTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var iconImageView: UIImageView!
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var labelBackground: UIView!
+    @IBOutlet weak var iconBackground: UIView!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+}
