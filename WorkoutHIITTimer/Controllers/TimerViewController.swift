28ff69c88bf4a0fe6a1bb50969d23cbfb4fb53d3
Warren Buckley <wbuckley@warrens-mbp.home>
2018-01-29 18:17:50 -0500

    Resize start button

--- WorkoutHIITTimer/Controllers/TimerViewController.swift
+++ WorkoutHIITTimer/Controllers/TimerViewController.swift
@@ -9,16 +9,19 @@
 import UIKit
 
 class TimerViewController: UIViewController {
-
+    
     @IBOutlet weak var startStopButton: UIButton!
     @IBOutlet weak var resetButton: UIButton!
     @IBOutlet weak var quitButton: UIButton!
     @IBOutlet weak var controlsContainerView: UIView!
+    @IBOutlet weak var controlsViewHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var startButtonHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var startButtonWidthConstraint: NSLayoutConstraint!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
@@ -28,12 +31,37 @@ class TimerViewController: UIViewController {
         createCircularButton(quitButton, color: UIColor(named: "TimerWhite"))
     }
     
-    private func createCircularButton(_ button: UIButton, color: UIColor?) {
+    private func createCircularButton(_ button: UIButton, color: UIColor?, width: CGFloat? = nil) {
         button.layer.borderColor = color?.cgColor
         button.backgroundColor = UIColor.clear
         button.layer.borderWidth = 2
-        button.layer.cornerRadius = button.frame.width / 2
+        button.layer.cornerRadius = width ?? button.frame.width / 2
         button.layer.masksToBounds = true
+    }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        DispatchQueue.main.async {
+            if UIDevice.current.orientation.isLandscape {
+                self.controlsViewHeightConstraint.constant = 150
+                self.startButtonHeightConstraint.constant = 60
+                self.startButtonWidthConstraint.constant = 60
+                self.startStopButton.titleLabel?.font = UIFont.systemFont(ofSize: 13)
+                 self.view.layoutIfNeeded()
+                self.createCircularButton(self.startStopButton, color: UIColor(named: "TimerOrange"), width: 60)
+            }
+            else {
+                self.controlsViewHeightConstraint.constant = 245
+                self.startButtonHeightConstraint.constant = 124
+                self.startButtonWidthConstraint.constant = 124
+                self.startStopButton.titleLabel?.font = UIFont.systemFont(ofSize: 20)
+                 self.view.layoutIfNeeded()
+                self.createCircularButton(self.startStopButton, color: UIColor(named: "TimerOrange"), width: 124)
+                
+            }
+           
+        }
+        //view.layoutIfNeeded()
     }
     
     override func didReceiveMemoryWarning() {
@@ -41,15 +69,15 @@ class TimerViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
